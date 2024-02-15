34456f5c2186601d616cead96f6db13b2fba1a00
watanabe-akihiro <k022g0118@g.neec.ac.jp>
2024-02-15 18:56:53 +0900

    完成

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <Novice.h>
+#include "MT.h"
+#include"Matrix4x4.h"
+#include"Vector3.h"
+#include"Vector2.h"
+#include<cmath>
+#include <cassert>
+#define _USE_MATH_DEFINES
 
-const char kWindowTitle[] = "LE2C_27_ワタナベアキヒロ";
+const char kWindowTitle[] = "LE2C_24_ワタナベ_アキヒロ";
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
@@ -9,13 +16,19 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
+
+	static const int kWindowWidth = 1280;
+	static const int kWindowHeght = 720;
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
 		// フレームの開始
 		Novice::BeginFrame();
+
+		Vector3 axis = Normalize({ 1.0f,1.0f,1.0f });
+		float angle = 0.44f;
 
 		// キー入力を受け取る
 		memcpy(preKeys, keys, 256);
@@ -24,8 +37,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓更新処理ここから
 		///
-
-
+		Matrix4x4 rotateMatrix = MakeRotateAxisAngle(axis, angle);
 		///
 		/// ↑更新処理ここまで
 		///
@@ -33,7 +45,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		///
 		/// ↓描画処理ここから
 		///
-
+		MatrixScreenPrintf(0, 0, rotateMatrix, "rotateMatrix");
 		///
 		/// ↑描画処理ここまで
 		///
